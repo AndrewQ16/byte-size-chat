7e00181ca9b64b615b72901256dea05503e19387
AndrewQ16 <afquinon@gmail.com>
2020-12-02 10:39:46 -0500

    Added names to users who who join and display when they'd connect or disconnect

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
-const { fileURLToPath } = require('url');
-
 var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+// gives access to files in our public folder
 app.use(express.static('public'));
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
+/**
+ * Current objective:
+ * 
+ */
+var connectedUsers = new Map();
 
+
+ // Here we are defining event listeners for the socket once a 'connection' is established
+io.on('connection', (socket) => {
+    socket.on('new-user', (packet) => {
+        connectedUsers.set(socket.id, {'name': packet.name});
+        io.emit('new-user', {'name': packet.name});
+        console.log(`${packet.name} has connected.`);
+    });
+    
     socket.on('disconnect', () => {
-        console.log('user disconnected');
+
+        let user = connectedUsers.get(socket.id);
+        io.emit('dc-user', {'socket_id': socket.id, 'name': user.name});
+        connectedUsers.delete(socket.id);
+        console.log(`${user.name} disconnected.`);
     });
 
+    // When the client emits a message
     socket.on('chat message', (msg) => {
-        console.log('message:' + msg);
         io.emit('chat message', msg);
     });
 });
--- public/js/main.js
+++ public/js/main.js
@@ -2,21 +2,57 @@ const message_form = document.getElementById('new-message');
 const socket = io();
 const messages = document.getElementById('messages');
 
-var nickname = prompt("Please enter your nickname", "Unknown");
+var nickname = "";
 
+/**
+ * When this client 'connect's to the server, emit a message to include this user's nickname
+ */
+socket.on('connect', ()=>{
+    nickname = prompt("Please enter your nickname", "Unknown");
+    socket.emit('new-user', {'name': nickname});
+    console.log(`emitted new-user: ${nickname}`);
+});
+
+
+
+/**
+ * When a 'new-user' message is emitted from the server, display the message on the html
+ */
+socket.on('new-user', (userDetails) =>{
+    let newUserMessage = document.createElement('li');
+    newUserMessage.innerText = `${userDetails.name} has connected`;
+    messages.appendChild(newUserMessage);
+});
+
+
+/**
+ * Event for submitting on the form
+ */
 message_form.addEventListener('submit',(e)=> {
     e.preventDefault();
     let message = document.getElementById('m')
+    // TODO: Trim the message
     if(message.value == ''){
         return;
     }
-    socket.emit('chat message', message.value);
+    socket.emit('chat message', {'name': nickname, 'msg': message.value});
     message.value = '';
 });
 
-socket.on('chat message', function(msg) {
+/**
+ * When the client receives a message
+ */
+socket.on('chat message', function(packet) {
     let message = document.createElement('li');
-    message.innerText = msg;
-    console.log('message:' + message.innerText);
+    message.innerText = `${packet.name}: ${packet.msg}`;
+    messages.append(message);
+});
+
+/**
+ * When the client receives a disconnected user event
+ */
+socket.on('dc-user', (packet) => {
+    let message = document.createElement('li');
+    message.innerText = `${packet.name} has disconnected`;
     messages.append(message);
 });
